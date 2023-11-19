ffafb235c1b4d7496333f8e7f3bfd42c3ab09c86
yr1404 <yash06765@gmail.com>
2023-11-19 13:02:08 +0530

    :bulb: Creates Landing Page

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,31 +1,45 @@
-import React from 'react';
-import './index.scss';
+import React, { useState } from 'react';
+import './styles.scss';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = () => {
+        setIsClicked(!isClicked);
+        console.log("clicked");
+    };
+
+
     return (
         <nav id="navbar">
             <div id="logo">
-                <Link to="">
+                <Link to="" id='logo-link'>
                     <img src="assets/logo 1.png" alt="Logo" />
+                    <span>eWaste Facility Locator</span>
                 </Link>
             </div>
             <ul className="nav-links-container">
                 <li className="navlinks">
                     <Link to="/locate">
-                        <img src="assets/locate.png" alt="" />
+                        <img src="assets/locate.png" alt="" id='locate' />
                     </Link>
                 </li>
                 <li className="navlinks">
                     <Link to="/credit">
-                        <img src="assets/coins.png" alt="" />
+                        <img src="assets/coins.png" alt="Credit Points" id='credit-points'/>
                     </Link>
                 </li>
                 <li className="navlinks">
-                    <Link to="/login">LogIn</Link>
+                    <Link to="/login" id='login-button'>Log In</Link>
                 </li>
                 <li className="navLinks">
-                    <img src="assets/search.png" alt="" />
+                    <img src="assets/search.png" alt=""
+                        onClick={handleClick} 
+                        style={{cursor:'pointer', zIndex:'15', position:'fixed', marginLeft:isClicked? '400px' : '410px', transition: 'all 0.3s ease'}}
+                    />
+                    <input type='text' id="search-bar" placeholder="Search" style={{opacity: isClicked ? 1 : 0, zIndex: isClicked ? 1 : 0}} />
                 </li>
             </ul>
         </nav>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.scss'
 
 const Home = () => {
+
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseOver = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseOut = () => {
+        setIsHovered(false);
+    };
+
+
+
     return (
         <div>
 
             <div className="container">
-                <img src="assets/background-electronic-devices 1.png" alt="bgImage" />
-                <img src="assets/recycle-img 1.png" alt="recycleImg" id='recycle-img' />
+                <img src="assets/background-electronic-devices 1.png" alt="bgImage" id='bg-image' 
+                    style={{opacity: isHovered ? 0 : 1}}
+                />
+                <img
+                    src="assets/recycle-img 1.png"
+                    alt="recycleImg"
+                    id='recycle-img'
+                    onMouseOver={handleMouseOver}
+                    onMouseOut={handleMouseOut}
+                />
+                <p className="recycle-text" style={{opacity: isHovered ? 1 : 0}}><span>Recycle Today</span>, For a Better Tomorrow</p>
             </div>
-            <p className="recycle-text">Recycle Today, For a Better Tomorrow</p>
         </div>
     )
 }
